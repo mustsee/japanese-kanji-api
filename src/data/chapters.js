3764a9ef20a45032d14c288edbb9169373deaf30
Thomas Sypniewski <thomas.sypniewski@gmail.com>
2023-03-28 13:16:25 +0200

    Add kanji from RTK chapter 2

--- src/data/chapters.js
+++ src/data/chapters.js
@@ -1,4 +1,4 @@
-import kanji from "./kanji.json";
+import kanji from "./kanji";
 
 const getKanji = (chars) => {
   let res = []
@@ -11,11 +11,11 @@ const getKanji = (chars) => {
 const chapters = {
   "1": {
     "name": "Nombres",
-    "kanji": getKanji(["人", "男"]),
+    "kanji": getKanji(["一", "二", "三", "四", "五", "六", "七", "八", "九", "十", "口", "日", "月", "田", "目"]),
   },
   "2": {
     "name": "Premières impressions",
-    "kanji": getKanji(["古", "吾"])
+    "kanji": getKanji(["古", "吾", "冒", "朋", "明", "唱", "晶", "品", "呂", "昌"])
   } 
 }
 
---
+++ src/data/kanji.js
@@ -0,0 +1,589 @@
+const kanji = {
+  一: {
+    kanji: "一",
+    readings: {
+      onyomi: [
+        {
+          reading: "イチ",
+          romaji: "ichi",
+        },
+      ],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["un"],
+      EN: ["one"],
+    },
+    // Mieux comprendre la différence entre composant/element et sésame/primitive
+    elements: {
+      FR: ["plancher", "plafond"],
+      EN: [],
+    },
+    summary: {
+      FR: "",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/一/index.html",
+  },
+  二: {
+    kanji: "二",
+    readings: {
+      onyomi: [
+        {
+          reading: "ニ",
+          romaji: "ni",
+        },
+      ],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["deux"],
+      EN: ["two"],
+    },
+    elements: {
+      FR: [],
+      EN: [],
+    },
+    summary: {
+      FR: "",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/二/index.html",
+  },
+  三: {
+    kanji: "三",
+    readings: {
+      onyomi: [
+        {
+          reading: "サン",
+          romaji: "san",
+        },
+      ],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["trois"],
+      EN: ["three"],
+    },
+    elements: {
+      FR: [],
+      EN: [],
+    },
+    summary: {
+      FR: "",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/三/index.html",
+  },
+  四: {
+    kanji: "四",
+    readings: {
+      onyomi: [
+        {
+          reading: "シ",
+          romaji: "shi",
+        },
+      ],
+      kunyomi: [
+        {
+          reading: "よん",
+          romaji: "yon",
+        },
+      ],
+    },
+    meanings: {
+      FR: ["quatre"],
+      EN: ["four"],
+    },
+    elements: {
+      FR: ["bouche", "paire de jambes"],
+      EN: [],
+    },
+    summary: {
+      FR: "",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/四/index.html",
+  },
+  五: {
+    kanji: "五",
+    readings: {
+      onyomi: [
+        {
+          reading: "ゴ",
+          romaji: "go",
+        },
+      ],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["cinq"],
+      EN: ["five"],
+    },
+    elements: {
+      FR: [],
+      EN: [],
+    },
+    summary: {
+      FR: "",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/五/index.html",
+  },
+  六: {
+    kanji: "六",
+    readings: {
+      onyomi: [
+        {
+          reading: "ロク",
+          romaji: "roku",
+        },
+      ],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["six"],
+      EN: ["six"],
+    },
+    elements: {
+      FR: ["chapeau haut de forme", "paire de pattes"],
+      EN: [],
+    },
+    summary: {
+      FR: "",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/六/index.html",
+  },
+  七: {
+    kanji: "七",
+    readings: {
+      onyomi: [
+        {
+          reading: "シチ",
+          romaji: "shichi",
+        },
+      ],
+      kunyomi: [
+        {
+          reading: "なな",
+          romaji: "nana",
+        },
+      ],
+    },
+    meanings: {
+      FR: ["sept"],
+      EN: ["seven"],
+    },
+    elements: {
+      FR: ["découper", "couper en dés"],
+      EN: [],
+    },
+    summary: {
+      FR: "",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/七/index.html",
+  },
+  八: {
+    kanji: "八",
+    readings: {
+      onyomi: [
+        {
+          reading: "ハチ",
+          romaji: "hachi",
+        },
+      ],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["huit"],
+      EN: ["eight"],
+    },
+    elements: {
+      FR: [],
+      EN: [],
+    },
+    summary: {
+      FR: "",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/八/index.html",
+  },
+  九: {
+    kanji: "九",
+    readings: {
+      onyomi: [
+        {
+          reading: "ク",
+          romaji: "ku",
+        },
+        {
+          reading: "キュウ",
+          romaji: "kyū",
+        },
+      ],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["neuf"],
+      EN: ["nine"],
+    },
+    elements: {
+      FR: ["nouveau"],
+      EN: [],
+    },
+    summary: {
+      FR: "",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/九/index.html",
+  },
+  十: {
+    kanji: "十",
+    readings: {
+      onyomi: [
+        {
+          reading: "jū",
+          romaji: "ジュウ",
+        },
+      ],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["dix"],
+      EN: ["ten"],
+    },
+    elements: {
+      FR: ["aiguille"],
+      EN: [],
+    },
+    summary: {
+      FR: "",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/十/index.html",
+  },
+  口: {
+    kanji: "口",
+    readings: {
+      onyomi: [],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["bouche"],
+      EN: [],
+    },
+    elements: {
+      FR: [],
+      EN: [],
+    },
+    summary: {
+      FR: "",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/口/index.html",
+  },
+  日: {
+    kanji: "日",
+    readings: {
+      onyomi: [],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["jour", "soleil"],
+      EN: [],
+    },
+    elements: {
+      FR: ["langue qui s'agite dans la bouche"],
+      EN: [],
+    },
+    summary: {
+      FR: "",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/日/index.html",
+  },
+  月: {
+    kanji: "月",
+    readings: {
+      onyomi: [],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["mois", "lune"],
+      EN: [],
+    },
+    elements: {
+      FR: ["chair", "partie du corps humain"],
+      EN: [],
+    },
+    summary: {
+      FR: "",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/月/index.html",
+  },
+  田: {
+    kanji: "田",
+    readings: {
+      onyomi: [],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["rizière"],
+      EN: [],
+    },
+    elements: {
+      FR: ["champ", "cerveau"],
+      EN: [],
+    },
+    summary: {
+      FR: "",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/田/index.html",
+  },
+  目: {
+    kanji: "目",
+    readings: {
+      onyomi: [],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["oeil"],
+      EN: [],
+    },
+    elements: {
+      FR: [],
+      EN: [],
+    },
+    summary: {
+      FR: "",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/目/index.html",
+  },
+  古: {
+    kanji: "古",
+    readings: {
+      onyomi: [],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["vieux"],
+      EN: [],
+    },
+    elements: {
+      FR: ["dix", "bouche"],
+      EN: [],
+    },
+    summary: {
+      FR: "Une pierre tombale surmontée d'une croix",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/古/index.html",
+  },
+  吾: {
+    kanji: "吾",
+    readings: {
+      onyomi: [],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["moi"],
+      EN: [],
+    },
+    elements: {
+      FR: ["cinq", "bouche"],
+      EN: [],
+    },
+    summary: {
+      FR: "Les cinq orifices de la tête, les cinq bouches : moi",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/吾/index.html",
+  },
+  冒: {
+    kanji: "冒",
+    readings: {
+      onyomi: [],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["risque"],
+      EN: [],
+    },
+    elements: {
+      FR: ["soleil", "oeil"],
+      EN: [],
+    },
+    summary: {
+      FR: "Regarder le soleil directement représente un risque de brûlure des yeux",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/冒/index.html",
+  },
+  朋: {
+    kanji: "朋",
+    readings: {
+      onyomi: [],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["compagnon"],
+      EN: [],
+    },
+    elements: {
+      FR: ["lune"],
+      EN: [],
+    },
+    summary: {
+      FR: "",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/朋/index.html",
+  },
+  朋: {
+    kanji: "朋",
+    readings: {
+      onyomi: [],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["compagnon"],
+      EN: [],
+    },
+    elements: {
+      FR: ["chair"],
+      EN: [],
+    },
+    summary: {
+      FR: "Le premier compagnon d'Adam fût Ève. En la voyant, il s'exclama : La chair de ma chair !",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/朋/index.html",
+  },
+  明: {
+    kanji: "明",
+    readings: {
+      onyomi: [],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["clair"],
+      EN: [],
+    },
+    elements: {
+      FR: ["soleil", "lune"],
+      EN: [],
+    },
+    summary: {
+      FR: "Le soleil se blottit contre la lune. Le rendez-vous impossible du soleil et de la lune.",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/明/index.html",
+  },
+  唱: {
+    kanji: "唱",
+    readings: {
+      onyomi: [],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["choeur"],
+      EN: [],
+    },
+    elements: {
+      FR: ["bouche", "langues qui s'agitent à l'intérieur"],
+      EN: [],
+    },
+    summary: {
+      FR: "Une bouche silencieuse (le chef de choeur), et deux bouches avec les langues qui s'agitent à l'intérieur (deux choristes, le nombre minimun pour former un choeur).",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/唱/index.html",
+  },
+  晶: {
+    kanji: "晶",
+    readings: {
+      onyomi: [],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["étinceler"],
+      EN: [],
+    },
+    elements: {
+      FR: ["soleil"],
+      EN: [],
+    },
+    summary: {
+      FR: "Ce kanji est l'image d'un soleil représenté à trois endroits, c'est à dire partout, et nous fournit ainsi l'image de quelque chose étincelant de toutes parts.",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/晶/index.html",
+  },
+  品: {
+    kanji: "品",
+    readings: {
+      onyomi: [],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["marchandises"],
+      EN: [],
+    },
+    elements: {
+      FR: ["bouche"],
+      EN: [],
+    },
+    summary: {
+      FR: "Le triplement d'un élément signifie partout ou en masse. Lorsque l'on pense à marchandises, on imagine des denrées produites en masses, c'est à dire pour les masses de bouches ouvertes.",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/品/index.html",
+  },
+  呂: {
+    kanji: "呂",
+    readings: {
+      onyomi: [],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["épine"],
+      EN: [],
+    },
+    elements: {
+      FR: [],
+      EN: [],
+    },
+    summary: {
+      FR: "",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/呂/index.html",
+  },
+  昌: {
+    kanji: "昌",
+    readings: {
+      onyomi: [],
+      kunyomi: [],
+    },
+    meanings: {
+      FR: ["prospère"],
+      EN: [],
+    },
+    elements: {
+      FR: ["soleil"],
+      EN: [],
+    },
+    summary: {
+      FR: "Puisqu'on parle des temps prospères comme des jours ensoleillés, que pourrait-il y avoir de plus prospère qu'un ciel avec deux soleils ?",
+      EN: "",
+    },
+    src: "https://hochanh.github.io/rtk/昌/index.html",
+  }
+}
+
+export default kanji;
